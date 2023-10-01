780f8ea24e80ba43e82d2f61f1e2abe52aaff546
Giannina <gftartaglia@gmail.com>
2023-10-01 20:41:52 -0300

    Finished with the logic and the aesthetics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,42 @@
-console.log("Hello World!");
+// Espera a que el documento HTML esté completamente cargado antes de ejecutar el código.
+document.addEventListener("DOMContentLoaded", () => {
+    // Obtiene la información de productos almacenada en el localStorage.
+    const productsInfo = JSON.parse(localStorage.getItem("productos"));
+    console.log(productsInfo); // Imprime la información de productos en la consola.
+
+    // Llama a la función createCards pasando la información de productos como argumento.
+    createCards(productsInfo);
+
+    // Realiza una solicitud fetch para obtener datos de productos desde un archivo "products.json".
+    fetch("products.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // Almacena los datos de productos obtenidos en el localStorage.
+            localStorage.setItem("productos", JSON.stringify(data.productos));
+        });
+
+    // Función para crear tarjetas de productos y mostrarlos en la página.
+    function createCards(data) {
+        card = "";
+        data.forEach(dato => {
+            // Crea una tarjeta de producto con los datos proporcionados.
+            card += `<div class="col-12 col-sm-6 col-lg-4 col-xl-3 my-1 d-flex align-items-stretch">
+                <div class="card p-2 m-2 shadow-sm bg-danger-subtle">
+                    <img src="${dato.imagen}" class="card-img-top" alt="${dato.nombre}">
+                    <div class="card-body text-center d-flex flex-column">
+                        <h5 class="card-title product-title">${dato.nombre}</h5>
+                        <p class="card-text text-muted">${dato.descripcion}</p>
+                        <div id="abajo" class="d-flex justify-content-md-between mt-auto flex-column">
+                            <p class="card-text p-2 text-muted">Precio: $${dato.precio}</p>                           
+                            <div>
+                                <a href="details.html?id=${dato.id}" class="btn btn-danger">VER MAS</a>
+                            </div>                    
+                        </div>                    
+                    </div>                   
+                </div>
+            </div>`;
+        })    
+        // Actualiza el contenido del contenedor de tarjetas con las tarjetas generadas.
+        cardsContainer.innerHTML = card;
+    }
+});
---
+++ assets/js/product.js
@@ -0,0 +1,37 @@
+// Espera a que el documento HTML esté completamente cargado antes de ejecutar el código.
+document.addEventListener("DOMContentLoaded", () => {
+  // Obtiene la referencia al contenedor HTML donde se mostrarán los detalles del producto.
+  const cardContainer = document.getElementById("details-card");
+
+  // Obtiene los parámetros de la URL, en este caso, el ID del producto de la página actual.
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get("id");
+
+  // Obtiene todos los productos almacenados en el almacenamiento local (localStorage).
+  const allProducts = JSON.parse(localStorage.getItem("productos"));
+
+  // Busca el producto correspondiente al ID en la lista de productos.
+  const productDetails = allProducts.find(
+      (product) => product.id === productId
+  );
+
+  // Si se encontraron detalles del producto, se muestra la información en el contenedor.
+  if (productDetails) {
+      cardContainer.innerHTML = `
+      <div class="row">
+          <div class="col-md-4">
+              <img src="${productDetails.imagen}" class="card-img m-md-4 py-2" alt="${productDetails.nombre}">
+          </div>
+          <div class="col-md-8">
+              <div class="card-body text-center">
+                  <h5 id="name" class="card-title pb-2">${productDetails.nombre}</h5>
+                  <p id="description" class="card-text">Producto #${productDetails.id}</p>
+                  <p id="description" class="card-text">${productDetails.descripcion}</p>
+                  <p id="details" class="card-text">${productDetails.detalle}</p>
+                  <p id="price" class="card-text">Precio: $${productDetails.precio}</p>
+                  <p id="stars" class="card-text">Valoración: ${productDetails.valoracion}</p>
+              </div>
+          </div>
+      </div>`;
+  }
+});
