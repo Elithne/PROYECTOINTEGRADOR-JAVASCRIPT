6ea86d3d5b7b638da0b1108bbef56ca4793877d7
Giannina <gftartaglia@gmail.com>
2023-10-01 20:53:07 -0300

    fixed an error

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const productsInfo = JSON.parse(localStorage.getItem("productos"));
     console.log(productsInfo); // Imprime la información de productos en la consola.
 
+    // Declara y define el elemento HTML que contendrá las tarjetas de productos.
+    const cardContainer = document.getElementById('cardsContainer');
+
     // Llama a la función createCards pasando la información de productos como argumento.
     createCards(productsInfo);
 
@@ -18,25 +21,29 @@ document.addEventListener("DOMContentLoaded", () => {
     // Función para crear tarjetas de productos y mostrarlos en la página.
     function createCards(data) {
         card = "";
-        data.forEach(dato => {
-            // Crea una tarjeta de producto con los datos proporcionados.
-            card += `<div class="col-12 col-sm-6 col-lg-4 col-xl-3 my-1 d-flex align-items-stretch">
-                <div class="card p-2 m-2 shadow-sm bg-danger-subtle">
-                    <img src="${dato.imagen}" class="card-img-top" alt="${dato.nombre}">
-                    <div class="card-body text-center d-flex flex-column">
-                        <h5 class="card-title product-title">${dato.nombre}</h5>
-                        <p class="card-text text-muted">${dato.descripcion}</p>
-                        <div id="abajo" class="d-flex justify-content-md-between mt-auto flex-column">
-                            <p class="card-text p-2 text-muted">Precio: $${dato.precio}</p>                           
-                            <div>
-                                <a href="details.html?id=${dato.id}" class="btn btn-danger">VER MAS</a>
+        if (data) {
+            data.forEach(dato => {
+                // Crea una tarjeta de producto con los datos proporcionados.
+                card += `<div class="col-12 col-sm-6 col-lg-4 col-xl-3 my-1 d-flex align-items-stretch">
+                    <div class="card p-2 m-2 shadow-sm bg-danger-subtle">
+                        <img src="${dato.imagen}" class="card-img-top" alt="${dato.nombre}">
+                        <div class="card-body text-center d-flex flex-column">
+                            <h5 class="card-title product-title">${dato.nombre}</h5>
+                            <p class="card-text text-muted">${dato.descripcion}</p>
+                            <div id="abajo" class="d-flex justify-content-md-between mt-auto flex-column">
+                                <p class="card-text p-2 text-muted">Precio: $${dato.precio}</p>                           
+                                <div>
+                                    <a href="details.html?id=${dato.id}" class="btn btn-danger">VER MAS</a>
+                                </div>                    
                             </div>                    
-                        </div>                    
-                    </div>                   
-                </div>
-            </div>`;
-        })    
-        // Actualiza el contenido del contenedor de tarjetas con las tarjetas generadas.
-        cardsContainer.innerHTML = card;
+                        </div>                   
+                    </div>
+                </div>`;
+            })    
+            // Actualiza el contenido del contenedor de tarjetas con las tarjetas generadas.
+            cardsContainer.innerHTML = card;
+        } else {
+            cardContainer.innerHTML = "No hay productos"; 
+        }    
     }
 });
